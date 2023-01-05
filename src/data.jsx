d4e97af34932f71d58f5a4ad630ca69c496e1334
samspuds <90201674+samspuds@users.noreply.github.com>
2023-01-05 14:32:46 +0000

    updates

--- src/data.jsx
+++ src/data.jsx
@@ -1,64 +1,64 @@
 export default [
   {
-    title: "Mount Fuji",
+    title: "Hà Giang, Vietnam",
     location: "Hai Giang",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    googleMapsUrl: "https://goo.gl/maps/DUv5qvuzRFyLYfoy6",
+    startDate: "12 June, 2022",
+    endDate: "17 June, 2022",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      "Ha Giang is a mountainous province in the North of Vietnam in which the Vietnam-China border extends more than 274km long. The province is also the source of Lo River flowing into Vietnam, contiguous to China to the north with international border gate Thanh Thuy. Southern of Ha Giang is Tuyen Quang; eastern is Cao Bang and Lao Cai to the West. This is where we commenced our 4 day biking journey.",
     imageUrl: "bikes.jpg",
   },
 
   {
     title: "Farm",
     location: "Ireland",
-    googleMapsUrl: "cow.jpg",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    googleMapsUrl: "https://goo.gl/maps/eyNU1vg8JZqtFX297",
+    startDate: "1 June, 2022",
+    endDate: "24 August, 2022",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      "During the summer month, turf is cut from the bog in East Galway, Ireland. It is left to dry for several months in fields until it can be collected and stored for the winter as you can see in this photo.",
     imageUrl: "cow.jpg",
   },
 
   {
     title: "Back Garden",
     location: "Ireland",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    googleMapsUrl: "https://goo.gl/maps/Kh1ybMHuQ1MF6TuBA",
+    startDate: "12 July, 2022",
+    endDate: "24 July, 2022",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      "A trip home to Galway with family during the summer months. Galway City is a thriving, bohemian, cultural city on the western coast of Ireland. Along with being a popular seaside destination with beautiful beaches and long winding promenade, it also has a buzzing cosmopolitan city centre. The city is a joy to explore with its labyrinthine cobbled streets, colourful shop facades and busy café/ bar culture.",
     imageUrl: "garden.jpg",
   },
   {
     title: "Stone Hands",
     location: "Da Nang",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    googleMapsUrl: "https://goo.gl/maps/c1QjjJX1MKKGc7WFA",
     startDate: "12 Jan, 2021",
     endDate: "24 Jan, 2021",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      "Just outside Danang, Vietnam, there is an amusement park located on top of mountain. You most take the worlds longest cable car to get to the top of this mountain. there is a bridge which is supported by two giant hands carved from stone here.",
     imageUrl: "hands.jpg",
   },
   {
-    title: "Hai Giang Loop",
+    title: "Hà Giang Loop",
     location: "Vietnam",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    googleMapsUrl: "https://goo.gl/maps/jhueRXFmurFqbNwh6",
+    startDate: "12 June, 2022",
+    endDate: "17 June, 2022",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      "Ha Giang loop is a circular route in the far north of Vietnam, on the border with China and is best explored on two wheels. You will drive through stunning scenery, gigantic mountains, glorious rivers, and stunning rice terraces. Ha Giang region simply has it all.",
     imageUrl: "rockl.jpg",
   },
   {
     title: "Winter Day",
     location: "Ireland",
-    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-    startDate: "12 Jan, 2021",
-    endDate: "24 Jan, 2021",
+    googleMapsUrl: "https://goo.gl/maps/Kh1ybMHuQ1MF6TuBA",
+    startDate: "12 Nov, 2022",
+    endDate: "24 Feb, 2023",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      "These are the signature moments of a winter in Galway, the time of year where the city is full of magic, mirth and endless things to do. Read on to see our list of favourite winter activities this season in Galway! Winter Shopping While wandering through the chilly cobblestone streets of the city, you'll find an endless sprawl of shops to pop into.",
     imageUrl: "sun.jpg",
   },
 ];
